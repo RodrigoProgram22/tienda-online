3fc3edf418be35b2d0c0945689fb78981f15c364
RodrigoProgram22 <rodrigoprograma22@gmail.com>
2023-03-03 01:36:31 -0300

    Seccion de ofertas agregado.

--- tienda-FrontEnd/src/app/app.module.ts
+++ tienda-FrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    OfertasComponent
   ],
   imports: [
     BrowserModule,
